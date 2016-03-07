2ec6dd8d3f5c19ed122d9411b7c7daae267a7698
Xanvier Brouwer <xanvier@tamtam.nl>
2016-03-07 18:35:55 +0100

    Add the ConsoleApplication for the hourly API update

---
+++ APIPoller/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Trade;
+using Trade.Models;
+using System.Configuration;
+using System.Net;
+using Newtonsoft.Json.Linq;
+
+namespace APIPoller
+{ 
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Groep 8 API poller");
+            DBCreate db = new DBCreate();
+            string APIurl = ConfigurationManager.AppSettings["APIurl"];
+            string App_id = ConfigurationManager.AppSettings["App_id"];
+
+            WebClient c = new WebClient();
+            var data = (new WebClient()).DownloadString("https://" + APIurl + "?app_id=" + App_id);
+            var CurrencyList = (JObject.Parse(data)).Last.First.ToList();
+            foreach(var Currency in CurrencyList)
+            {
+                Forex f = new Forex();
+                f.Code = ((Newtonsoft.Json.Linq.JProperty)Currency).Name.ToString();
+                f.Ratio = Convert.ToDouble(((Newtonsoft.Json.Linq.JProperty)Currency).Value.ToString());
+
+                AddNewForex(db, f);
+            }
+        }
+
+        private static void AddNewForex(DBCreate db, Forex forex)
+        {
+            db.Forexes.Add(forex);
+            db.SaveChanges();
+        }
+    }
+}
