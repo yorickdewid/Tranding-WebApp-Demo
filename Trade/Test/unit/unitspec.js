33a6bfd15f3a9be3369a6028fd6be8cd85928835
Remco Sellmeijer <Remco.sellmeijer@gmail.com>
2016-03-10 18:03:26 +0100

    Added unit test test

---
+++ Trade/Test/unit/unitspec.js
@@ -0,0 +1,22 @@
+﻿describe('ForexController', function () {
+    beforeEach(module('forexControllers'));
+    beforeEach(module('forexServices'));
+
+    var $controller;
+    beforeEach(inject(function (_$controller_) {
+        $controller = _$controller_;
+    }));
+
+    describe("The existence of ForexCtr", function () {
+        it('should have ForexCtr', function () {
+            var $scope = {};
+            var controller = $controller('ForexCtrl', { $scope: $scope });
+            expect($controller).toBeDefined();
+        });
+
+        it('should have ForexService', inject(function (Forex) {
+            expect(Forex).toBeDefined();
+        }));
+    });
+
+}); 
